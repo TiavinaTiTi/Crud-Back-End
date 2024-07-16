1237244d1b0233309fd4890c3cd2cced8072099f
TiavinaTiTi <titus50231@gmail.com>
2024-07-16 10:28:18 +0300

    Creer un API rest

---
+++ src/main/java/mlg/project/portfolio/controllers/PersonController.java
@@ -0,0 +1,26 @@
+package mlg.project.portfolio.controllers;
+
+import mlg.project.portfolio.models.Person;
+import mlg.project.portfolio.views.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin(origins = "*")
+@RestController
+public class PersonController {
+    @Autowired
+    PersonRepository personRepository;
+
+
+    @GetMapping("/person")
+    public List<Person> getAllPerson(){
+        return this.personRepository.findAll();
+    }
+
+    @PostMapping("/post")
+    public Person postPerson(@RequestBody Person person){
+        return this.personRepository.save(person);
+    }
+}
